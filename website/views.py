49829ed778792780ec62761a21b592f078c39631
Mahdi-Ostadrahim <mahdi.ostadrahim@gmail.com>
2022-09-28 17:41:40 +0330

    HW6-1 Complete
    Part 1 of HW6 complete

--- website/views.py
+++ website/views.py
@@ -1,13 +1,15 @@
+from datetime import datetime
+from time import strptime
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-
+from website.models import Post
+from django.utils import timezone
 def index_view(request):
     return render(request,'website/index.html')
-
-def about_view(request):
-    return render(request,'website/about.html')
-
-def contact_view(request):
-    
-    return render(request,'website/contact.html')
-
+def HW(request):
+    posts = Post.objects.exclude(published_date__gt=datetime.now())
+    context = {'posts': posts}
+    for post in posts:
+        post.counted_view += 1
+        post.save()
+    return render(request,'website/HW.html',context)
